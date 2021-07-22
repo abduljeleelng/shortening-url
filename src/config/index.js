d9cef57a5d9cbe35457de5dac7a04702104d4c3e
abduljeleelng <abduljeleelng@gmail.com>
2021-07-22 14:53:15 +0100

    configure DB base on the Node environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,6 @@
 const mongoose = require('mongoose');
 const port = process.env.PORT || 9000;
 
-const dbConnection = async ()=>{
-    mongoose
-    .connect(process.env.MONGO_URI, {useCreateIndex: true,useUnifiedTopology:true, useNewUrlParser:true, keepAlive:true,})
-    .then(() => console.log(`Database is connected on port ${port}`));
-    mongoose.connection.on("error", err => {
-        console.log(`DB connection error: ${err.message}`);
-    }); 
-}
-
-
 function connect() {
     return new Promise((resolve, reject) => {
       if (process.env.NODE_ENV==="test") {
@@ -19,7 +9,7 @@ function connect() {
         mockgoose.helper.setDbVersion("3.2.1");
         return mockgoose.prepareStorage()
           .then(() => {
-              mongoose.connect(process.env.MONGO_URI, 
+              mongoose.connect(process.env.MONGO_TEST, 
                 {useCreateIndex: true,useUnifiedTopology:true, useNewUrlParser:true, keepAlive:true,})
                   .then((res, err) => {
                       console.log("TEST = DOCUMENT ")
@@ -28,7 +18,7 @@ function connect() {
                   })
           })
       }else if(process.env.NODE_ENV === 'dev'){
-          return mongoose.connect(process.env.MONGO_URI,
+          return mongoose.connect(process.env.MONGO_DEV,
             {useCreateIndex: true,useUnifiedTopology:true, useNewUrlParser:true, keepAlive:true,})
               .then((res, err) => {
                   console.log("DEV = DOCUMENT")
@@ -36,7 +26,7 @@ function connect() {
                 resolve();
             })
       }else if(process.env.NODE_ENV === 'pro'){
-          return mongoose.connect(process.env.MONGO_URI,
+          return mongoose.connect(process.env.MONGO_PRO,
             {useCreateIndex: true,useUnifiedTopology:true, useNewUrlParser:true, keepAlive:true,})
               .then((res, err) => {
                 console.log("PRO= DOCUEMT")
@@ -59,6 +49,3 @@ function close() {
 }
 
 module.exports = { connect, close };
-
-
-//module.exports = dbConnection
